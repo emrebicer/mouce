26b8f0a64fa93512b48fcbba1c1c59cb786a96ae
Emre Bicer <oebicer@gmail.com>
2024-12-08 15:06:59 +0300

    add a development flake for pinning dependency versions

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Mouce development flake";
+
+  inputs = {
+    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, ... }@inputs: 
+
+    let 
+      system = "x86_64-linux";
+      pkgs = nixpkgs-unstable.legacyPackages.${system};
+    in {
+        devShells.${system}.default = pkgs.mkShellNoCC {
+          buildInputs = with pkgs; [
+              gcc13
+              xorg.libX11
+              xorg.libXtst
+              cargo
+              rustc
+              rust-analyzer
+          ];
+        };
+
+    };
+}

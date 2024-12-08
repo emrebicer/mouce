f68a31454b64f11c8d1909d215cfbc0ad06d9ef5
Emre Bicer <oebicer@gmail.com>
2024-12-08 15:07:26 +0300

    update tests to prevent running in parallel by default

--- src/common.rs
+++ src/common.rs
@@ -14,7 +14,7 @@ pub enum ScrollDirection {
     Up,
     Down,
     Right,
-    Left
+    Left,
 }
 
 #[derive(Debug, Copy, Clone)]
@@ -169,174 +169,212 @@ pub trait MouseActions {
 mod tests {
     use crate::error::Error;
     use crate::{common::MouseButton, common::ScrollDirection, Mouse};
+    use std::sync::Mutex;
     use std::{thread, time};
+
+    // Prevent test running in parallel,
+    static TEST_EXECUTER: Mutex<Executor> = Mutex::new(Executor);
+    #[derive(Clone, Copy)]
+    struct Executor;
+
+    impl Executor {
+        fn run_test(self, f: impl FnOnce()) {
+            f();
+        }
+    }
 
     #[test]
     #[ignore]
     fn move_to_right_bottom() {
-        let manager = Mouse::new();
-        assert_eq!(manager.move_to(1920, 1080), Ok(()));
+        TEST_EXECUTER.lock().unwrap().run_test(|| {
+            let manager = Mouse::new();
+            assert_eq!(manager.move_to(1920, 1080), Ok(()));
+        });
     }
 
     #[test]
     #[ignore]
     fn move_to_top_left() {
-        let manager = Mouse::new();
-        assert_eq!(manager.move_to(0, 0), Ok(()));
+        TEST_EXECUTER.lock().unwrap().run_test(|| {
+            let manager = Mouse::new();
+            assert_eq!(manager.move_to(0, 0), Ok(()));
+        });
     }
 
     #[test]
     #[ignore]
     fn move_to_left_to_right() {
-        let manager = Mouse::new();
-        let sleep_duration = time::Duration::from_millis(1);
-        let mut x = 0;
-        while x < 1920 {
-            assert_eq!(manager.move_to(x, 540), Ok(()));
-            x += 1;
-            thread::sleep(sleep_duration);
-        }
+        TEST_EXECUTER.lock().unwrap().run_test(|| {
+            let manager = Mouse::new();
+            let sleep_duration = time::Duration::from_millis(1);
+            let mut x = 0;
+            while x < 1920 {
+                assert_eq!(manager.move_to(x, 540), Ok(()));
+                x += 1;
+                thread::sleep(sleep_duration);
+            }
+        });
     }
 
     #[test]
     #[ignore]
     fn move_relative_left_to_right() {
-        let manager = Mouse::new();
-        let sleep_duration = time::Duration::from_millis(1);
-        let mut x = 0;
-        assert_eq!(manager.move_to(0, 540), Ok(()));
-        while x < 1920 {
-            assert_eq!(manager.move_relative(1, 0), Ok(()));
-            x += 1;
-            thread::sleep(sleep_duration);
-        }
+        TEST_EXECUTER.lock().unwrap().run_test(|| {
+            let manager = Mouse::new();
+            let sleep_duration = time::Duration::from_millis(1);
+            let mut x = 0;
+            assert_eq!(manager.move_to(0, 540), Ok(()));
+            while x < 1920 {
+                assert_eq!(manager.move_relative(1, 0), Ok(()));
+                x += 1;
+                thread::sleep(sleep_duration);
+            }
+        });
     }
 
     #[test]
     #[ignore]
     fn move_to_top_to_bottom() {
-        let manager = Mouse::new();
-        let sleep_duration = time::Duration::from_millis(1);
-        let mut y = 0;
-        while y < 1080 {
-            assert_eq!(manager.move_to(960, y), Ok(()));
-            y += 1;
-            thread::sleep(sleep_duration);
-        }
+        TEST_EXECUTER.lock().unwrap().run_test(|| {
+            let manager = Mouse::new();
+            let sleep_duration = time::Duration::from_millis(1);
+            let mut y = 0;
+            while y < 1080 {
+                assert_eq!(manager.move_to(960, y), Ok(()));
+                y += 1;
+                thread::sleep(sleep_duration);
+            }
+        });
     }
 
     #[test]
     #[ignore]
     fn move_relative_top_to_bottom() {
-        let manager = Mouse::new();
-        let sleep_duration = time::Duration::from_millis(1);
-        let mut y = 0;
-        assert_eq!(manager.move_to(960, 0), Ok(()));
-        while y < 1080 {
-            assert_eq!(manager.move_relative(0, 1), Ok(()));
-            y += 1;
-            thread::sleep(sleep_duration);
-        }
+        TEST_EXECUTER.lock().unwrap().run_test(|| {
+            let manager = Mouse::new();
+            let sleep_duration = time::Duration::from_millis(1);
+            let mut y = 0;
+            assert_eq!(manager.move_to(960, 0), Ok(()));
+            while y < 1080 {
+                assert_eq!(manager.move_relative(0, 1), Ok(()));
+                y += 1;
+                thread::sleep(sleep_duration);
+            }
+        });
     }
 
     #[test]
     #[ignore]
     fn get_position() {
-        let manager = Mouse::new();
-        match manager.get_position() {
-            Ok(_) => {
-                let positions = vec![
-                    (0, 0),
-                    (100, 100),
-                    (250, 250),
-                    (325, 325),
-                    (400, 100),
-                    (100, 400),
-                ];
+        TEST_EXECUTER.lock().unwrap().run_test(|| {
+            let manager = Mouse::new();
+            match manager.get_position() {
+                Ok(_) => {
+                    let positions = vec![
+                        (0, 0),
+                        (100, 100),
+                        (250, 250),
+                        (325, 325),
+                        (400, 100),
+                        (100, 400),
+                    ];
 
-                let mut x;
-                let mut y;
-                for position in positions.iter() {
-                    assert_eq!(manager.move_to(position.0, position.1), Ok(()));
-                    (x, y) = manager.get_position().unwrap();
-                    assert_eq!(x, position.0 as i32);
-                    assert_eq!(y, position.1 as i32);
+                    let mut x;
+                    let mut y;
+                    for position in positions.iter() {
+                        assert_eq!(manager.move_to(position.0, position.1), Ok(()));
+                        (x, y) = manager.get_position().unwrap();
+                        assert_eq!(x, position.0 as i32);
+                        assert_eq!(y, position.1 as i32);
+                    }
                 }
+                Err(error) => assert_eq!(error, Error::NotImplemented),
             }
-            Err(error) => assert_eq!(error, Error::NotImplemented),
-        }
+        });
     }
 
     #[test]
     #[ignore]
     fn left_click() {
-        let manager = Mouse::new();
-        assert_eq!(manager.click_button(&MouseButton::Left), Ok(()));
+        TEST_EXECUTER.lock().unwrap().run_test(|| {
+            let manager = Mouse::new();
+            assert_eq!(manager.click_button(&MouseButton::Left), Ok(()));
+        });
     }
 
     #[test]
     #[ignore]
     fn scroll_down() {
-        let manager = Mouse::new();
-        for _ in 0..10 {
-            assert_eq!(manager.scroll_wheel(&ScrollDirection::Down), Ok(()));
-            let sleep_duration = time::Duration::from_millis(250);
-            thread::sleep(sleep_duration);
-        }
+        TEST_EXECUTER.lock().unwrap().run_test(|| {
+            let manager = Mouse::new();
+            for _ in 0..10 {
+                assert_eq!(manager.scroll_wheel(&ScrollDirection::Down), Ok(()));
+                let sleep_duration = time::Duration::from_millis(250);
+                thread::sleep(sleep_duration);
+            }
+        });
     }
 
     #[test]
     #[ignore]
     fn scroll_up() {
-        let manager = Mouse::new();
-        for _ in 0..10 {
-            assert_eq!(manager.scroll_wheel(&ScrollDirection::Up), Ok(()));
-            let sleep_duration = time::Duration::from_millis(250);
-            thread::sleep(sleep_duration);
-        }
+        TEST_EXECUTER.lock().unwrap().run_test(|| {
+            let manager = Mouse::new();
+            for _ in 0..10 {
+                assert_eq!(manager.scroll_wheel(&ScrollDirection::Up), Ok(()));
+                let sleep_duration = time::Duration::from_millis(250);
+                thread::sleep(sleep_duration);
+            }
+        });
     }
 
     #[test]
     #[ignore]
     fn scroll_right() {
-        let manager = Mouse::new();
-        for _ in 0..10 {
-            assert_eq!(manager.scroll_wheel(&ScrollDirection::Right), Ok(()));
-            let sleep_duration = time::Duration::from_millis(250);
-            thread::sleep(sleep_duration);
-        }
+        TEST_EXECUTER.lock().unwrap().run_test(|| {
+            let manager = Mouse::new();
+            for _ in 0..10 {
+                assert_eq!(manager.scroll_wheel(&ScrollDirection::Right), Ok(()));
+                let sleep_duration = time::Duration::from_millis(250);
+                thread::sleep(sleep_duration);
+            }
+        });
     }
 
     #[test]
     #[ignore]
     fn scroll_left() {
-        let manager = Mouse::new();
-        for _ in 0..10 {
-            assert_eq!(manager.scroll_wheel(&ScrollDirection::Left), Ok(()));
-            let sleep_duration = time::Duration::from_millis(250);
-            thread::sleep(sleep_duration);
-        }
+        TEST_EXECUTER.lock().unwrap().run_test(|| {
+            let manager = Mouse::new();
+            for _ in 0..10 {
+                assert_eq!(manager.scroll_wheel(&ScrollDirection::Left), Ok(()));
+                let sleep_duration = time::Duration::from_millis(250);
+                thread::sleep(sleep_duration);
+            }
+        });
     }
 
     #[test]
     #[ignore]
     fn hook_and_unhook() {
-        let mut manager = Mouse::new();
-        assert_eq!(manager.unhook(5), Err(Error::UnhookFailed));
-        let hook_result = manager.hook(Box::new(|e| println!("{:?}", e)));
-        match hook_result {
-            Ok(id) => {
-                assert_eq!(manager.unhook(id), Ok(()));
+        TEST_EXECUTER.lock().unwrap().run_test(|| {
+            let mut manager = Mouse::new();
+            assert_eq!(manager.unhook(5), Err(Error::UnhookFailed));
+            let hook_result = manager.hook(Box::new(|e| println!("{:?}", e)));
+            match hook_result {
+                Ok(id) => {
+                    assert_eq!(manager.unhook(id), Ok(()));
 
-                manager.hook(Box::new(|e| println!("{:?}", e))).unwrap();
-                manager.hook(Box::new(|e| println!("{:?}", e))).unwrap();
-                manager.hook(Box::new(|e| println!("{:?}", e))).unwrap();
-                let id = manager.hook(Box::new(|e| println!("{:?}", e))).unwrap();
+                    manager.hook(Box::new(|e| println!("{:?}", e))).unwrap();
+                    manager.hook(Box::new(|e| println!("{:?}", e))).unwrap();
+                    manager.hook(Box::new(|e| println!("{:?}", e))).unwrap();
+                    let id = manager.hook(Box::new(|e| println!("{:?}", e))).unwrap();
 
-                manager.unhook_all().unwrap();
-                assert_eq!(manager.unhook(id), Err(Error::UnhookFailed));
+                    manager.unhook_all().unwrap();
+                    assert_eq!(manager.unhook(id), Err(Error::UnhookFailed));
+                }
+                Err(err) => assert_eq!(Error::PermissionDenied, err),
             }
-            Err(err) => assert_eq!(Error::PermissionDenied, err),
-        }
+        });
     }
 }
